a65850832cca2fd662df60ff187f02dc60141bfa
anuragdevon <msm19b021@iiitdm.ac.in>
2023-04-01 09:34:38 +0530

    setup blueprint teacher resources

--- core/apis/assignments/teacher.py
+++ core/apis/assignments/teacher.py
@@ -4,9 +4,9 @@ from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
 
-from .schema import AssignmentSchema
-
+from .schema import AssignmentSchema, AssignmentGradeSchema
 teacher_assignments_resources = Blueprint('teacher_assignments_resources', __name__)
+
 
 @teacher_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
 @decorators.auth_principal
