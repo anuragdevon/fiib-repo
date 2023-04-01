cb7f45e6cf73c96f85f6ee6a11ffb0054afc0cc1
anuragdevon <msm19b021@iiitdm.ac.in>
2023-04-01 10:30:21 +0530

    setup assignments grading

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -80,4 +80,15 @@ class Assignment(db.Model):
     def get_assignments_to_teachers(cls, teacher_id):
         return cls.filter(cls.teacher_id == teacher_id).all()
 
+    @classmethod
+    def grade_assignment(cls, _id, grade, principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED)
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment belongs comes under other teacher')
+        assertions.assert_valid(grade in [member.value for member in GradeEnum])
         
+        assignment.grade = grade
+        db.session.flush()
+
+        return assignment
